a272b20b94f99ff8aa1208ecdbc2b0e5fea3e2fe
supermarcos <supermarcos@gmail.com>
2016-02-15 16:56:36 +1000

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var togle_drawer = function() {
+  debugger;
+  var drawer = document.querySelector('.mdl-layout');
+  if(drawer.classList.contains("is-small-screen")) {
+    drawer.MaterialLayout.drawerToggleHandler_();
+  }git
+};
